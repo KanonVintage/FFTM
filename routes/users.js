ae83eda5921a6d6b188a33133f89373b9b50db9f
KanonVintage <guido.duchi@gmail.com>
2016-09-22 03:26:41 -0500

    gosh! Operario is done! finally for **** sake

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ module.exports = function(app, passport) {
 
 var User = require('../models/user.js');
 var Sample = require('../models/sample.js');
+var Exam = require('../models/exam.js');
 
 	// =====================================
 	// TO UPDATE AND DELETE USERS ==========
@@ -55,6 +56,7 @@ var Sample = require('../models/sample.js');
 	    Sample
 	    .find()
 	    .populate('_user')
+	    .populate('results')
 	    .exec(function(err, samples, next){
 	        if(err){
 	            return next(err);
@@ -113,7 +115,92 @@ var Sample = require('../models/sample.js');
 	//DELETE - Eliminar sample
 	app.delete('/samples/:id', isLoggedIn, function(req, res){
 		if(req.user.type=="operario"){
-			Sample.findByIdAndRemove(req.params.id, function(err){
+			Sample.findByIdAndRemove(req.params.id, function(err, sample){
+				for (var i=0;i<sample.results.length;i++){
+					Exam.findByIdAndRemove(sample.results[i], function(err,exam){
+						if(err){res.send(err)}
+					})
+				}
+				if(err){res.send(err)}
+				res.json({message: 'That one is gone'});
+			})
+		}else{
+            return res.redirect('/');
+        }
+	})
+
+
+	// =====================================
+	// TO ADD UPDATE AND DELETE EXAMS ======
+	// =====================================
+	app.get('/exams'/*, isLoggedIn*/, function(req, res){ 
+	    Exam
+	    .find()
+	    .populate({path: '_sample', populate:{path:'_user'}})
+	    .exec(function(err, exams, next){
+	        if(err){
+	            return next(err);
+	        }else {
+	            res.json(exams);    
+	        }
+	    });
+	});
+
+	app.post('/exam', isLoggedIn, function(req,res){
+		if(true){
+			var exam = new Exam({
+				_sample			 : req.body.id,
+				type             : req.body.type,
+			    parameter        : req.body.parameter,
+			    unit             : req.body.unit,
+			    result           : req.body.result,
+			    ref              : req.body.ref
+			});
+			exam.save(function(err){
+				Sample.findById(req.body.id, function(err, sample){
+					sample.results.push(exam);
+					sample.save(function(err){
+						if(err){}
+					})
+				})
+				if(err){res.send(err)}
+				res.json(exam);
+			});
+		}else{
+			res.json('Something went wrong')
+            return res.redirect('/');
+        }
+	})
+
+	//PUT - Actualizar exam
+	app.put('/exams/:id', isLoggedIn, function(req, res){
+		if(true){
+			Exam.findById(req.params.id, function(err, exam){
+				exam.type     	    = req.body.type,
+			    exam.parameter	    = req.body.parameter,
+			    exam.unit           = req.body.unit,
+			    exam.result         = req.body.result,
+			    exam.ref       	    = req.body.ref
+				exam.save(function(err){
+					if(err){res.send(err)}
+					res.json(exam);
+				})
+			})
+		}else{
+            return res.redirect('/');
+        }
+	})
+
+	//DELETE - Eliminar exam
+	app.delete('/exams/:id', isLoggedIn, function(req, res){
+		if(req.user.type=="laboratorista"){
+			Exam.findByIdAndRemove(req.params.id, function(err,exam){
+				Sample.findById(exam._sample, function(err, sample){
+					sample.results.pull(exam);
+					sample.save(function(err){
+						if(err){}
+					})
+				})
 				if(err){res.send(err)}
 				res.json({message: 'That one is gone'});
 			})
